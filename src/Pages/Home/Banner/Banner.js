6703329fce8e8c4288d69e97beea43bfbde315cd
mhselim103 <mhselin103@gmail.com>
2021-11-22 01:55:58 +0600

    carousel modify

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -8,45 +8,33 @@ const Banner = () => {
         <Carousel.Item interval={1000}>
           <img
             className="d-block img"
-            src="https://i.ibb.co/4tPbQTQ/coronavirus-awereness.png"
+            src="https://i.ibb.co/fXgpKbK/coronavirus-awereness1.png"
             alt="First slide"
           />
-          <Carousel.Caption className="carousel-caption">
-            <div className="bg-primary text-light">
-              <h3>Coronavirus (COVID-19) Vaccination camp</h3>
-              {/* <p>
-                Everything you need to know about our screening clinics, visitor
-                restrictions and getting vaccinated at our COVID-19 vaccination
-                centers.
-              </p> */}
-            </div>
-          </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item interval={500}>
           <img
             className="d-block img"
-            src="https://i.ibb.co/ZS876gF/survivor.png"
+            src="https://i.ibb.co/w7bM1Tr/survivor1.png"
             alt="Second slide"
           />
-          <Carousel.Caption className="carousel-caption">
+          {/* <Carousel.Caption className="carousel-caption">
             <div className="bg-primary text-light">
               <h3>Help victorians survive life's most critical moments.</h3>
-              {/* <p>Donate to the nobel cure</p> */}
             </div>
-          </Carousel.Caption>
+          </Carousel.Caption> */}
         </Carousel.Item>
         <Carousel.Item>
           <img
             className="d-block img"
-            src="https://i.ibb.co/s5yb1JM/victorians.png"
+            src="https://i.ibb.co/rvcY7tC/victorians1.png"
             alt="Third slide"
           />
-          <Carousel.Caption className="carousel-caption">
+          {/* <Carousel.Caption className="carousel-caption">
             <div className="bg-primary text-light">
               <h3>From survivor to inspiration and Advocate</h3>
-              {/* <p>A celebration to human spirit</p> */}
             </div>
-          </Carousel.Caption>
+          </Carousel.Caption> */}
         </Carousel.Item>
       </Carousel>
     </div>
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -3,49 +3,53 @@ import { Container, Nav, Navbar } from "react-bootstrap";
 import { HashLink } from "react-router-hash-link";
 import useAuth from "../../../Hooks/useAuth";
 const Header = () => {
-  const { user } = useAuth();
+  const { user, logOut } = useAuth();
   console.log(user.email);
   return (
-    <div>
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand>Noble Cure</Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="mx-auto">
-              <Nav.Link as={HashLink} to="/home">
-                Home
+    <Navbar
+      className="sticky-top"
+      collapseOnSelect
+      expand="lg"
+      bg="dark"
+      variant="dark"
+    >
+      <Container>
+        <Navbar.Brand>Noble Cure</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mx-auto">
+            <Nav.Link as={HashLink} to="/home">
+              Home
+            </Nav.Link>
+            <Nav.Link as={HashLink} to="/services">
+              Services
+            </Nav.Link>
+            <Nav.Link as={HashLink} to="/home">
+              About Us
+            </Nav.Link>
+          </Nav>
+          <Nav>
+            {!user.email ? (
+              <Nav.Link as={HashLink} to="/login">
+                Log In
               </Nav.Link>
-              <Nav.Link as={HashLink} to="/services">
-                Services
+            ) : (
+              <Nav.Link>
+                {" "}
+                <span>{user.displayName}</span>{" "}
               </Nav.Link>
-              <Nav.Link as={HashLink} to="/home">
-                About Us
+            )}
+            {!user.email ? (
+              <Nav.Link as={HashLink} to="/register">
+                Sign Up
               </Nav.Link>
-            </Nav>
-            <Nav>
-              {!user.email ? (
-                <Nav.Link as={HashLink} to="/login">
-                  Log In
-                </Nav.Link>
-              ) : (
-                <Nav.Link>
-                  {" "}
-                  <span>{user.displayName}</span>{" "}
-                </Nav.Link>
-              )}
-              {!user.email ? (
-                <Nav.Link as={HashLink} to="/register">
-                  Sign Up
-                </Nav.Link>
-              ) : (
-                <Nav.Link>Log Out</Nav.Link>
-              )}
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </div>
+            ) : (
+              <Nav.Link onClick={logOut}>Log Out</Nav.Link>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
