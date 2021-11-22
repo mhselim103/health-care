1dfd1888b363edb55ba3a7c1ed8fc29c96501e84
Mahmudul Hasan Selim <mhselim103@gmail.com>
2021-11-23 01:17:22 +0600

    form added

--- src/Pages/Authenticatin/Login/Login.js
+++ src/Pages/Authenticatin/Login/Login.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { Button } from "react-bootstrap";
+import { Button, Form } from "react-bootstrap";
 import { Link, useLocation, useHistory } from "react-router-dom";
 import useAuth from "../../../Hooks/useAuth";
+import "./Login.css";
 
 const LogIn = () => {
   const location = useLocation();
@@ -18,27 +19,54 @@ const LogIn = () => {
       })
       .finally(() => setIsLoading(false));
   };
+  const handleClick = (e) => {
+    e.preventDefault();
+  };
   return (
-    <div className="banner d-flex justify-content-center">
+    <div className="d-flex justify-content-center">
       <div>
-        <h1>Log In</h1>
-        <form>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            placeholder="your email"
-          />
-          <br />
-          <input type="password" name="password" id="password" />
-        </form>
-        <p>
-          New to Noble Cure? <Link to="/register">Register</Link>
+        <h3 className="text-center">Log In Here</h3>
+        <Form>
+          <Form.Group className="my-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" required />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" required />
+          </Form.Group>
+          {/* <p className="bg-primary w-100 text-center text-light p-2 cursour rounded-3">
+            Sign In
+          </p> */}
+          <Button
+            variant="primary"
+            type="submit"
+            className="w-100"
+            onClick={handleClick}
+          >
+            Submit
+          </Button>
+        </Form>
+        <p className="text-center">
+          New to Noble Cure?{" "}
+          <Link className="register" to="/register">
+            Register
+          </Link>
         </p>
-        <div>--------------or------------</div>
-        <Button variant="primary" onClick={handleSignIn}>
-          Google SignIn
-        </Button>
+        <div>
+          ---------------------------------------------
+          <p className="text-center">Or Sign In with</p>
+          <h3>
+            <i className="fab fa-facebook text-center bg-dark text-light w-100 cursour rounded-3"></i>
+          </h3>
+          <h3>
+            <i className="fab fa-google text-center bg-dark text-light w-100 cursour rounded-3"></i>
+          </h3>
+          <h3>
+            <i className="fab fa-github text-center bg-dark text-light w-100 cursour rounded-3"></i>
+          </h3>
+        </div>
       </div>
     </div>
   );
--- src/Pages/Authenticatin/Register/Register.js
+++ src/Pages/Authenticatin/Register/Register.js
@@ -1,43 +1,62 @@
 import React from "react";
-import { Button } from "react-bootstrap";
+import { Button, Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import useAuth from "../../../Hooks/useAuth";
 
 const Register = () => {
   const { signInUsingGoogle } = useAuth();
+  const handleClick = (e) => {
+    e.preventDefault();
+  };
   return (
     <div className="banner d-flex justify-content-center">
       <div>
-        <h1>Create Account</h1>
-        <form>
-          <input
-            type="text"
-            name="yourName"
-            id="yourName"
-            placeholder="Your Name"
-          />
-          <br />
-          <input
-            type="email"
-            name="email"
-            id="email"
-            placeholder="your email"
-          />
-          <br />
-          <input
-            type="password"
-            name="password"
-            id="pasword"
-            placeholder="your password"
-          />
-        </form>
+        <h3 className="text-center">Create Your Account</h3>
+        <Form>
+          <Form.Group className="my-3" controlId="formBasicName">
+            <Form.Label>Full Name</Form.Label>
+            <Form.Control type="Text" placeholder="Enter Your Name" required />
+          </Form.Group>
+          <Form.Group className="my-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" required />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" required />
+          </Form.Group>
+          {/* <p className="bg-primary w-100 text-center text-light p-2 cursour rounded-3">
+            Sign Up
+          </p> */}
+          <Button
+            variant="primary"
+            type="submit"
+            className="w-100"
+            onClick={handleClick}
+          >
+            Submit
+          </Button>
+        </Form>
         <br />
         <p>Already have an account? </p>
         <Link to="/login">Log In Here</Link>
-        <div>--------------or------------</div>
-        <Button variant="primary" onClick={signInUsingGoogle}>
-          Google SignIn
-        </Button>
+        <div>
+          ---------------------------------------------
+          <p className="text-center">Or Sign In with</p>
+          <h3>
+            <i className="fab fa-facebook text-center bg-dark text-light w-100 cursour rounded-3"></i>
+          </h3>
+          <h3>
+            <i
+              onClick={signInUsingGoogle}
+              className="fab fa-google text-center bg-dark text-light w-100 cursour rounded-3"
+            ></i>
+          </h3>
+          <h3>
+            <i className="fab fa-github text-center bg-dark text-light w-100 cursour rounded-3"></i>
+          </h3>
+        </div>
       </div>
     </div>
   );
