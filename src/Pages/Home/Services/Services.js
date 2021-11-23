82ace548a671feb4f3d3677e5410e0cf564184cd
Mahmudul Hasan Selim <mhselim103@gmail.com>
2021-11-24 00:28:55 +0600

    responsive correction

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -10,7 +10,7 @@ const Services = () => {
       <div className="mt-5">
         <h1 className="text-center">Our Services</h1>
       </div>
-      <div className="row row-cols-2 row-cols-md-4 g-4 mt-5 text-center ">
+      <div className="row row-cols-1 row-cols-md-4 g-4 mt-5 text-center ">
         {services?.map((service) => (
           <Service key={service.id} singleService={service}></Service>
         ))}
